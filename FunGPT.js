88a0ec8d301276c978cd27371f75396f23f0f899
Furkan Nabi Sumji <103491179+furkannabisumji@users.noreply.github.com>
2023-08-27 15:17:46 +0530

    initial commit

---
+++ FunGPT.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const TelegramBot = require('node-telegram-bot-api');
+const OpenAI = require('openai');
+require('dotenv').config();
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+const openai = new OpenAI({ apiKey: process.env.OPENAI_TOKEN });
+
+class Message {
+    constructor(content) {
+        this.role = "user";
+        this.content = content;
+    }
+}
+
+bot.on('message', async (msg) => {
+    const messageArray = JSON.parse(fs.readFileSync('messages.json', 'utf8'))
+    messageArray.push(new Message(msg.text));
+    const updatedJson = JSON.stringify(messageArray);
+
+    fs.writeFileSync('messages.json', updatedJson);
+
+    const completion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        max_tokens: 1500,
+        messages: messageArray
+    });
+    bot.sendMessage(msg.chat.id, completion.choices[0].message.content);
+});
